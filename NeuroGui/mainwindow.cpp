243b5b1d2e02565c4983fdcac5aa4a1482b6aa88
tanvirt <tanvirt@ufl.edu>
2014-09-09 17:43:57 -0400

    GUI starting point

---
+++ NeuroGui/mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    ui->label->setText("Channels: ");
+    ui->horizontalSlider->setMaximum(8);
+    ui->horizontalSlider->setMinimum(0);
+    ui->spinBox->setMaximum(8);
+    ui->spinBox->setMinimum(0);
+
+    connect(ui->horizontalSlider, SIGNAL(valueChanged(int)), ui->spinBox, SLOT(setValue(int)));
+    connect(ui->spinBox, SIGNAL(valueChanged(int)), ui->horizontalSlider, SLOT(setValue(int)));
+
+    ui->pushButton->setText("Start");
+    ui->pushButton_2->setText("Stop");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
